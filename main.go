baea5cbf59aad910f59813e2b27054be531bcc19
Kushal <121809555+KushalJangra@users.noreply.github.com>
2025-01-21 11:07:48 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+
 package main
 
 import (
@@ -5,10 +6,10 @@ import (
     "encoding/json"
     "fmt"
     "log"
-    "math/rand"
+    // "math/rand"
     "net/http"
-    "strconv"
-    "time"
+    // "strconv"
+    // "time"
 
     _ "github.com/go-sql-driver/mysql"
     "github.com/gorilla/mux"
@@ -18,25 +19,11 @@ const (
     DBHost  = "127.0.0.1"
     DBUser  = "root"
     DBPass  = "Kush@123456"
-    DBDbase = "sdb"
+    DBDbase = "pro"
     PORT    = ":3000"
 )
 
 var database *sql.DB
-
-type Customer struct {
-    ID    int    `json:"id"`
-    Name  string `json:"name"`
-    Email string `json:"email"`
-}
-
-type Product struct {
-    ID   int    `json:"id"`
-    Name string `json:"name"`
-}
-type JSONResponse struct {
-    Fields map[string]string `json:"fields"`
-}
 
 func initDB() {
     dbConn := fmt.Sprintf("%s:%s@tcp(%s)/%s", DBUser, DBPass, DBHost, DBDbase)
@@ -46,146 +33,170 @@ func initDB() {
     }
 
     database = db
-    // Test the connection
+   
     if err := database.Ping(); err != nil {
         log.Fatalf("Database ping error: %v", err)
     }
     log.Println("Database connected successfully!")
 }
 
-// APICommentPost handles the POST request for adding comments
-func APICustPost(w http.ResponseWriter, r *http.Request) {
-    var commentAdded bool
-    resp := JSONResponse{Fields: make(map[string]string)}
-
-    err := r.ParseForm()
-    if err != nil {
-        http.Error(w, "Unable to parse form", http.StatusBadRequest)
-        return
-    }
-
-    name := r.FormValue("name")
-    email := r.FormValue("email")
-    comments := r.FormValue("comments")
-    guid := r.FormValue("guid")
-
-    rand.Seed(time.Now().UnixNano())
-    randomInt := rand.Intn(1001)
-
-    res, err := database.Exec("INSERT INTO customer (page_id, comment_name, comment_email, comment_text, comment_guid) VALUES (?, ?, ?, ?, ?)", randomInt, name, email, comments, guid)
-    if err != nil {
-        http.Error(w, "Failed to add comment", http.StatusInternalServerError)
-        log.Println(err)
-        return
-    }
-
-    id, err := res.LastInsertId()
-    if err != nil {
-        commentAdded = false
-        log.Println(err)
-    } else {
-        commentAdded = true
-    }
-
-    // Prepare JSON response
-    resp.Fields["id"] = strconv.FormatInt(id, 10)
-    resp.Fields["added"] = strconv.FormatBool(commentAdded)
-
-    w.Header().Set("Content-Type", "application/json")
-    jsonResp, _ := json.Marshal(resp)
-    w.WriteHeader(http.StatusOK)
-    w.Write(jsonResp)
+type Product struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
 }
 
-// cust get
-func APIcustget(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    cID := vars["id"]
 
-    cus := Customer{}
-    err := database.QueryRow("SELECT page_id, comment_name, comment_email, comment_text, comment_guid FROM customer WHERE page_id=?", cID).
-        Scan(&cus.ID, &cus.Name, &cus.Email)
-    if err != nil {
-        http.Error(w, " not found", http.StatusNotFound)
-        log.Println("not found", cID)
-        return
-    }
-
-    w.Header().Set("Content-Type", "application/json")
-    json.NewEncoder(w).Encode(cus)
+type Customer struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
 }
 
-func APIprod(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    pID := vars["id"]
 
-    prods := Product{}
-    err := database.QueryRow("SELECT p_id, o_name FROM prod WHERE p_id=?", pID).
-        Scan(&prods.ID, &prods.Name)
-    if err != nil {
-        http.Error(w, "Order not found", http.StatusNotFound)
-        log.Println("Couldn't find order with ID:", pID)
-        return
-    }
+func CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var product Product
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		http.Error(w, "Invalid input", http.StatusBadRequest)
+		return
+	}
 
-    w.Header().Set("Content-Type", "application/json")
-    json.NewEncoder(w).Encode(prods)
+	query := "INSERT INTO product (name) VALUES (?)"
+	result, err := database.Exec(query, product.Name)
+	if err != nil {
+		http.Error(w, "Error creating product", http.StatusInternalServerError)
+		return
+	}
+
+	id, _ := result.LastInsertId()
+	product.ID = int(id)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(product)
 }
 
-func APIprodPost(w http.ResponseWriter, r *http.Request) {
-    var prodadded bool
+func GetProducts(w http.ResponseWriter, r *http.Request) {
+	query := "SELECT id, name FROM product"
+	rows, err := database.Query(query)
+	if err != nil {
+		http.Error(w, "Error fetching products", http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
 
-    resp := JSONResponse{Fields: make(map[string]string)}
+	var products []Product
+	for rows.Next() {
+		var product Product
+		if err := rows.Scan(&product.ID, &product.Name); err != nil {
+			http.Error(w, "Error scanning product row", http.StatusInternalServerError)
+			return
+		}
+		products = append(products, product)
+	}
 
-    err := r.ParseForm()
-    if err != nil {
-        http.Error(w, "Unable to parse form", http.StatusBadRequest)
-        return
-    }
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(products)
+}
 
-    name := r.FormValue("name")
+func CreateCustomer(w http.ResponseWriter, r *http.Request) {
+	var customer Customer
+	err := json.NewDecoder(r.Body).Decode(&customer)
+	if err != nil {
+		http.Error(w, "Invalid input", http.StatusBadRequest)
+		return
+	}
 
-    rand.Seed(time.Now().UnixNano())
-    randomInt := rand.Intn(1001)
+	query := "INSERT INTO customer (name, email) VALUES (?, ?)"
+	result, err := database.Exec(query, customer.Name, customer.Email)
+	if err != nil {
+		http.Error(w, "Error creating customer", http.StatusInternalServerError)
+		return
+	}
 
-    res, err := database.Exec("INSERT INTO prod (p_id, p_name) VALUES ( ?, ?)", randomInt, name)
-    if err != nil {
-        http.Error(w, "Failed to add order", http.StatusInternalServerError)
-        log.Println(err)
-        return
-    }
+	id, _ := result.LastInsertId()
+	customer.ID = int(id)
 
-    id, err := res.LastInsertId()
-    if err != nil {
-        prodadded = false
-        log.Println(err)
-    } else {
-        prodadded = true
-    }
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(customer)
+}
 
-    resp.Fields["id"] = strconv.FormatInt(id, 10)
-    resp.Fields["added"] = strconv.FormatBool(prodadded)
+func GetCustomers(w http.ResponseWriter, r *http.Request) {
+	query := "SELECT id, name, email FROM customer"
+	rows, err := database.Query(query)
+	if err != nil {
+		http.Error(w, "Error fetching customers", http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
 
-    w.Header().Set("Content-Type", "application/json")
-    jsonResp, _ := json.Marshal(resp)
-    w.WriteHeader(http.StatusOK)
-    w.Write(jsonResp)
+	var customers []Customer
+	for rows.Next() {
+		var customer Customer
+		if err := rows.Scan(&customer.ID, &customer.Name, &customer.Email); err != nil {
+			http.Error(w, "Error scanning customer row", http.StatusInternalServerError)
+			return
+		}
+		customers = append(customers, customer)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(customers)
+}
+
+func GetProductByID(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	var product Product
+	query := "SELECT id, name FROM product WHERE id = ?"
+	err := database.QueryRow(query, id).Scan(&product.ID, &product.Name)
+	if err == sql.ErrNoRows {
+		http.Error(w, "Product not found", http.StatusNotFound)
+		return
+	} else if err != nil {
+		http.Error(w, "Error fetching product", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(product)
+}
+
+func GetCustomerByID(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	var customer Customer
+	query := "SELECT id, name, email FROM customer WHERE id = ?"
+	err := database.QueryRow(query, id).Scan(&customer.ID, &customer.Name, &customer.Email)
+	if err == sql.ErrNoRows {
+		http.Error(w, "Customer not found", http.StatusNotFound)
+		return
+	} else if err != nil {
+		http.Error(w, "Error fetching customer", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(customer)
 }
 
 func main() {
-    
-    initDB()
-    defer database.Close()
 
-    
-    r := mux.NewRouter()
+	initDB()
+	defer database.Close()
 
-    r.HandleFunc("/api/cust", APICustPost).Methods("POST")
-    r.HandleFunc("/api/cust/{id:[0-9]+}", APIcustget).Methods("GET").Schemes("http")
+	r := mux.NewRouter()
 
-    r.HandleFunc("/api/prod/{id:[0-9]+}", APIprod).Methods("GET").Schemes("http")
-    r.HandleFunc("/api/prod", APIprodPost).Methods("POST")
+	
+	r.HandleFunc("/products", CreateProduct).Methods("POST")
+	r.HandleFunc("/products", GetProducts).Methods("GET")
+	r.HandleFunc("/customers", CreateCustomer).Methods("POST")
+	r.HandleFunc("/customers", GetCustomers).Methods("GET")
+	r.HandleFunc("/products/{id:[0-9]+}", GetProductByID).Methods("GET")
+	r.HandleFunc("/customers/{id:[0-9]+}", GetCustomerByID).Methods("GET")
 
-    log.Printf("Server running on http://localhost%s", PORT)
-    log.Fatal(http.ListenAndServe(PORT, r))
+
+	fmt.Println("Server running on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
